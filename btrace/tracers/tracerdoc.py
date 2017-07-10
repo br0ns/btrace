1479d48d03c7267b486154a57fceb344a0a038ab
Morten Brøns-Pedersen <f@ntast.dk>
2017-07-10 09:35:39 +0200

    changed function order in `TracerDoc`

--- btrace/tracers/tracerdoc.py
+++ btrace/tracers/tracerdoc.py
@@ -3,16 +3,30 @@ class TracerDoc(object):
     tracer API.
     '''
 
+    def on_birth(self, tracee):
+        pass
+
+    def on_death(self, tracee, status):
+        pass
+
+    def on_kill(self, tracee):
+        pass
+
+
+    def on_stop(self, tracee):
+        pass
+
     def on_syscall(self, tracee, syscall, args):
+        '''This function is called when a tracee performs a syscall'''
+        pass
+
+    def on_syscall_return(self, tracee, syscall, retval):
         pass
 
     def on_read(self, tracee, args):
         pass
 
     def on_read_return(self, tracee, retval):
-        pass
-
-    def on_syscall_return(self, tracee, syscall, retval):
         pass
 
     def on_repid(self, tracee, oldpid):
@@ -28,16 +42,4 @@ class TracerDoc(object):
         pass
 
     def on_signal(self, tracee, siginfo):
-        pass
-
-    def on_stop(self, tracee):
-        pass
-
-    def on_death(self, tracee, status):
-        pass
-
-    def on_kill(self, tracee):
-        pass
-
-    def on_birth(self, tracee):
         pass
